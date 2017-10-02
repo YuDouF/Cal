f9f600a44a93b6cf99ca372e772e7142b998f73d
Mr.Right <mr.right@MrRights-iMac.local>
2017-10-02 10:43:44 +0800

    Initial Commit

---
+++ Cal/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cal
+//
+//  Created by Mr.Right on 2017/10/2.
+//  Copyright © 2017年 Mr.Right. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
